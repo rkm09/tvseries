e854a9ac82b0da8501f3b021c60d177d8826fb86
rkm09 <raksha.mishra09@gmail.com>
2026-06-14 08:07:14 +0530

    unit tests

--- src/main/java/com/restapi/tvseries/TvSeriesApplication.java
+++ src/main/java/com/restapi/tvseries/TvSeriesApplication.java
@@ -1,13 +1,7 @@
 package com.restapi.tvseries;
 
-import com.restapi.tvseries.exception.ExternalApiException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class TvSeriesApplication {
